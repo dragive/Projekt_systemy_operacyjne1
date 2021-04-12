9ec7df77796aa1316199fce535038d4405d82e86
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2021-04-12 20:58:41 +0200

    Refactoring

--- src/filehandleTest.c
+++ src/filehandleTest.c
@@ -1,3 +1,13 @@
+/**
+ * @file filehandleTest.c
+ * @author Krzysztof Funkowski
+ * @brief File for testing functionalities
+ * @version 0.1
+ * @date 2021-03-22
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,45 +19,10 @@
 #include <pthread.h>
 #include "file.h"
 #include "commandEntity.h"
-
-void* threading_func(void* vargp)
-{
-    int i,pid,child_pid,time;
-    command_struct* command = (command_struct*)vargp;
-    char** string_array=split(command->command,2,' ');
-    char ch[1];
-    /*sscanf(string_array[1],"%d",&i);
-    printf("i: %d\n",i);
-    i=5;
-    sprintf(ch,"%d",i);*/
-    char *args[] = {string_array[0],string_array[1],NULL};
-    pid = fork();
-        if(pid == 0)
-        {
-
-            child_pid = fork();
-            if(child_pid == 0)
-            {
-                execlp("sleep","sleep",string_array[1], NULL);
-                exit(0);
-            }
-            else
-            {
-                //printf("CHILD CHILD END\n");
-                wait(NULL);
-            }
-            //printf("CHILD END\n");
-            execvp(string_array[0],args);
-            exit(0);
-        }
-        else
-        {
-            wait(NULL);
-            //printf("PARENT END\n");
-        }
+#include "multithreading.h"
 
 
-}
+
 
 
 
---
+++ src/multithreading.h
@@ -0,0 +1,20 @@
+/**
+ * @file dayTime.h
+ * @author Krzysztof Funkowski
+ * @brief File to convert time
+ * @version 0.1
+ * @date 2021-03-22
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#ifndef MULTITHREADING_H_INCLUDED
+#define MULTITHREADING_H_INCLUDED
+
+
+/** \brief Threading function
+ */
+void* threading_func(void* vargp);
+
+
+#endif // MULTITHREADING_H_INCLUDED
